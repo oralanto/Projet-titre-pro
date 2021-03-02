39344ae4dfff50208d4a155548c345cd9c699372
KaioFernandes <kaio.fernandesdev@gmail.com>
2021-03-02 15:24:19 +0100

    Add React-model and components header/footer

---
+++ FRONT/src/components/App/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import Header from 'src/components/Header';
+import Footer from 'src/components/Footer';
+import './styles.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <Header />
+    <Footer />
+  </div>
+);
+
+// == Export
+export default App;
