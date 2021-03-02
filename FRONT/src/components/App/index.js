42ec5b4fee3cad9ee110b417c5cb1f1fd2c6631f
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-03-02 22:37:16 +0100

    Modification Footer style

--- FRONT/src/components/App/index.js
+++ FRONT/src/components/App/index.js
@@ -4,18 +4,18 @@ import React from 'react';
 // == Import
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-// import Home from 'src/components/Home';
+import Home from 'src/components/Home';
 // import LegalNotes from 'src/components/LegalNotes';
-import AboutUs from 'src/components/AboutUs';
+// import AboutUs from 'src/components/AboutUs';
 import './styles.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
-    {/* <Home /> */}
+    <Home />
     {/* <LegalNotes /> */}
-    <AboutUs />
+    {/* <AboutUs /> */}
     <Footer />
   </div>
 );
