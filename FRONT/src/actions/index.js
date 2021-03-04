6bd12bfa75bccdda3765cb1f3ef352355203ce10
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-03-04 19:54:33 +0100

    action LOGIN

--- FRONT/src/actions/index.js
+++ FRONT/src/actions/index.js
@@ -1,9 +1,14 @@
 // Action types
 export const CHANGE_INPUT_VALUE = 'CHANGE_INPUT_VALUE';
+export const LOGIN = 'LOGIN';
 
 // Action creators
 export const changeInputValue = (value, name) => ({
   type: CHANGE_INPUT_VALUE,
   value,
   name,
+});
+
+export const login = () => ({
+  type: LOGIN,
 });
