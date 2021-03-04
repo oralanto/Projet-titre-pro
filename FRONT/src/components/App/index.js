798e49fd02be8f20448ab4510c2520c9758515e0
KaioFernandes <kaio.fernandesdev@gmail.com>
2021-03-04 09:07:59 +0100

    Userdashboard done

--- FRONT/src/components/App/index.js
+++ FRONT/src/components/App/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 // == Import
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-// import Home from 'src/components/Home';
+import Home from 'src/components/Home';
 // import LegalNotes from 'src/components/LegalNotes';
 // import AboutUs from 'src/components/AboutUs';
 // import Contact from 'src/components/Contact';
