d032655984606aa9231057a7ae9a72e35601a40d
Mathis-web <mathis.pati@gmail.com>
2021-03-08 00:46:50 +0100

    connection to online database or local database

--- BACK/app/database.js
+++ BACK/app/database.js
@@ -5,6 +5,18 @@ const { Pool } = require('pg');
 // PGUSER pour l'utilisateur
 // PGPASSWORD pour le mot de passe
 //PGDATABASE pour la base de données
-const db = new Pool();
+let db;
+
+if(process.env.ENVIRONNEMENT === 'developpement') {
+    db = new Pool();
+} else if (process.env.ENVIRONNEMENT === 'production') {
+    db = new Pool({
+        user: process.env.RDS_USERNAME,
+        host: process.env.RDS_HOSTNAME,
+        database: process.env.RDS_DATABASE,
+        password: process.env.RDS_PASSWORD,
+        port: process.env.RDS_PORT,
+    })
+}
 
 module.exports = db;
