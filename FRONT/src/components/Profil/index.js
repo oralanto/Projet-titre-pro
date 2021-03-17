f4cac99b8296b745991e031bcd1fb87783a615da
KaioFernandes <kaio.fernandesdev@gmail.com>
2021-03-17 08:38:29 +0100

    profil user in progress

--- FRONT/src/components/Profil/index.js
+++ FRONT/src/components/Profil/index.js
@@ -1,32 +1,65 @@
 // == Import npm
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 // == Import
 import reactLogo from './react-logo.svg';
+
 import './style.scss';
 
 // == Composant
-const Profil = () => (
-  <div className="profil">
-    <div className="profil__picture">
-      <h1>Profil</h1>
-      <img className="profil__picture__img" src={reactLogo} alt="react logo" />
-    </div>
-    <div className="profil__info">
-      <p> Pseudo</p>
-      <input className="profil__info__bar" placeholder=" Ecrivez ici..." type="text" />
-      <p> Nom</p>
-      <input className="profil__info__bar" placeholder=" Ecrivez ici..." type="text" />
-      <p> Prenom</p>
-      <input className="profil__info__bar" placeholder=" Ecrivez ici..." type="text" />
-      <p> Adresse</p>
-      <input className="profil__info__bar" placeholder=" Ecrivez ici..." type="text" />
-      <p> Numero de telephone</p>
-      <input className="profil__info__bar" placeholder=" Ecrivez ici..." type="text" />
-    </div>
+const Profil = () => {
+  const [profil, setProfil] = useState([]);
 
-  </div>
-);
+  useEffect(() => {
+    axios
+      .get('http://34.207.234.22/api/profil')
+      .then((res) => setProfil(res.data));
+  }, []);
+
+  return (
+    <div className="profil">
+      <div className="profil__picture">
+        <h1>Profil</h1>
+        <img className="profil__picture__img" src={reactLogo} alt="react logo" />
+      </div>
+      <div className="profil__info">
+        <div className="profil__info__h2"> Pseudo</div>
+        <div className="profil__info__bar" />
+        <div className="profil__info__h2"> Nom</div>
+        <span className="profil__info__bar" />
+        <div className="profil__info__h2"> Prenom</div>
+        <span className="profil__info__bar" />
+        <div className="profil__info__h2"> Adresse</div>
+        <span className="profil__info__bar" />
+        <div className="profil__info__h2"> Numero de telephone</div>
+        <span className="profil__info__bar" />
+      </div>
+      <div className="profil__links">
+        <Link
+          to="/modifier-profil"
+          key={profil.id}
+          className="profil__info__buttons"
+        > Modifier Profil
+        </Link>
+        <Link
+          to="/user-adverts"
+          key={profil.id}
+          className="profil__info__buttons"
+        > Modifier ses Annonces
+        </Link>
+        <Link
+          to="/supprimer-profil"
+          key={profil.id}
+          className="profil__info__buttons"
+        > Supprimer son profil
+        </Link>
+      </div>
+
+    </div>
+  );
+};
 
 // == Export
 export default Profil;
