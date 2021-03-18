1db702a5e7e185da38d1d90b7941545df4b601d3
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-03-18 10:33:21 +0100

    Correction component Home

--- FRONT/src/components/Home/index.js
+++ FRONT/src/components/Home/index.js
@@ -13,7 +13,15 @@ const Home = () => (
     </div>
     <div className="home__center">
       <div className="home__text">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.
+        </p>
       </div>
       <div className="home__logo">
         <img className="home__logo" src="https://i.ibb.co/DGJchjY/oboardgame-logo.png" alt="logo" />
@@ -33,5 +41,3 @@ const Home = () => (
 
 // == Export
 export default Home;
-
-
