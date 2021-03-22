fb848247a5a0ea447debb0b2b722fd5c1b681714
Osee-Ralantoarison <o.ralanto@gmail.com>
2021-03-22 09:30:37 +0100

    CSS correction contact

--- FRONT/src/components/Contact/index.js
+++ FRONT/src/components/Contact/index.js
@@ -19,34 +19,33 @@ const Contact = ({
       <h1 className="Contact__title">Contactez-nous</h1>
       <form className="Contact__form" onSubmit={thisOnSubmit}>
         <Field
-          className="Contact__form__title"
+          className="Contact__form__field"
           type="text"
           label="Nom"
           name="lastname"
         />
         <Field
-          className="Contact__form__title"
+          className="Contact__form__field"
           type="text"
           label="Prénom"
           name="firstname"
         />
         <Field
-          className="Contact__form__title"
+          className="Contact__form__field"
           type="text"
           label="Adresse mail"
           name="email"
         />
         <Field
-          className="Contact__form__title"
+          className="Contact__form__field"
           type="text"
           label="Numéro de téléphone"
           name="phoneNumber"
         />
-        <Field
-          className="Contact__form__title"
-          type="textarea"
-          label="Message"
-          name="message"
+        <textarea
+          className="Contact__form__textarea"
+          rows="10"
+          placeholder="Laisser votre message"
         />
         <input className="Contact__form__button" type="submit" value="Envoyer votre message" />
       </form>
